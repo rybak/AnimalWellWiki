f2d570ddea1baed1682d1bf6bf9062a31af339ef
Andrei Rybak <rybak.a.v@gmail.com>
2024-08-25 19:05:56 +0200

    aw_images: skip 'Template:Copyright missing' and 'Template:Delete'
    Add two more templates to the list of templates, which we skip, if they
    are present on a file's page.
    
    - https://animalwell.wiki.gg/wiki/Template:Copyright_missing
    - https://animalwell.wiki.gg/wiki/Template:Delete

--- bot/aw_images.py
+++ bot/aw_images.py
@@ -124,7 +124,7 @@ def main(*args):
         return True
 
 
-    templates_ready = ['Copyright game', 'CC0', 'CC-BY-SA-3.0']
+    templates_ready = ['Copyright game', 'CC0', 'CC-BY-SA-3.0', 'Copyright missing', 'Delete']
     site = pywikibot.Site()
     looked_at = set()
     # https://animalwell.wiki.gg/api.php?action=query&list=allpages&apnamespace=6
